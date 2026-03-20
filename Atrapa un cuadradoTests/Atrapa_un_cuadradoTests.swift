069c8b403534e20274c291cd374c35b777c30031
hermoso92 <antoniohermoso92@gmail.com>
2026-03-20 01:03:57 +0100

    Initial Commit

---
+++ Atrapa un cuadradoTests/Atrapa_un_cuadradoTests.swift
@@ -0,0 +1,17 @@
+//
+//  Atrapa_un_cuadradoTests.swift
+//  Atrapa un cuadradoTests
+//
+//  Created by Antonio Hermoso on 20/3/26.
+//
+
+import Testing
+@testable import Atrapa_un_cuadrado
+
+struct Atrapa_un_cuadradoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
---
+++ Atrapa un cuadradoUITests/Atrapa_un_cuadradoUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  Atrapa_un_cuadradoUITestsLaunchTests.swift
+//  Atrapa un cuadradoUITests
+//
+//  Created by Antonio Hermoso on 20/3/26.
+//
+
+import XCTest
+
+final class Atrapa_un_cuadradoUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
